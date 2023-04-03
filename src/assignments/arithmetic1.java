d499fce89575eff6f7c832505a2c684910c6fe66
VikasChavan <VikasChavan@DESKTOP-UIDLFHA>
2023-04-03 16:46:10 +0530

    Arithmetic operator 1

---
+++ src/assignments/arithmetic1.java
@@ -0,0 +1,13 @@
+package assignments;
+
+public class arithmetic1 {
+	public static void main(String[] args) {
+	int i = 10;
+
+	int j = i++ + i++ + --i + ++i + ++i; 
+
+	System.out.println(i);
+
+	System.out.println(j);
+	}
+}
