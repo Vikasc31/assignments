7ba656053621c17a4483afe6c6ca59dbcd60cd04
VikasChavan <VikasChavan@DESKTOP-UIDLFHA>
2023-04-03 18:06:27 +0530

    output: 12,22

---
+++ src/assignments/arithmetic4.java
@@ -0,0 +1,16 @@
+package assignments;
+
+public class arithmetic4 {
+	
+	public static void main(String[] args) {
+
+		int i = 10;
+
+		int j = i++ + ++i; 
+
+		System.out.println(i); 
+
+		System.out.println(j);
+
+	}
+}
