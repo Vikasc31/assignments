e072cb83d2ca839d62c6d563e23fc3cf1495239f
VikasChavan <VikasChavan@DESKTOP-UIDLFHA>
2023-04-03 18:03:08 +0530

    output: 11,10

---
+++ src/assignments/arithmetic3.java
@@ -0,0 +1,16 @@
+package assignments;
+
+public class arithmetic3 {
+	
+	public static void main(String[] args) {
+
+		int i = 10;
+
+		int j = i++; 
+
+		System.out.println(i); 
+
+		System.out.println(j);
+
+	}
+}
