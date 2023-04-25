50fba23e53aba3f13d25868f1b650342ae2dd111
VikasChavan <VikasChavan@DESKTOP-UIDLFHA>
2023-04-25 17:05:52 +0530

    sum of all digits which entered by user.

---
+++ src/assignments/SumAll.java
@@ -0,0 +1,26 @@
+package assignments;
+import java.util.*;
+public class SumAll {
+	
+	public static void main(String[] args) {
+		
+		int num, num2,sum = 0;
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Enter any digit");
+		
+		num = s.nextInt();
+		
+		while(num > 0)
+		{
+			num2 = num % 10;
+			
+			sum = sum + num2;
+			
+			num = num / 10;
+		}
+		System.out.println("Sum = " +sum);
+	}
+
+}
